0ec53173b36ce1aff50b8b7c372db6dbfde7876e
Wafula Wanjala <wafulawanjala78@gmail.com>
2025-01-19 15:38:32 +0300

    initial commit

--- quiz.js
+++ quiz.js
@@ -13,28 +13,3 @@ function checkAnswer() {
 
     const submitButton = document.getElementById("submit-answer");
     submitButton.addEventListener("click", checkAnswer);
-
-
-
-
-/*let number1 = 2;
-let number2 = 2
-function add(number1, number2){
-    console.log(number1+number2);
-}
-add(number1,number2);
-
-function subtract(number1,number2){
-    console.log(number1-number2);
-}
-subtract(number1,number2);
-
-function divide(number1, number2){
-    console.log(number1/number2)
-}
-divide(number1,number2);
-
-function multiply(number1, number2){
-    console.log(number1*number2)
-}
-multiply(number1,number2);*/
