4c21f0eb6db3fa320fe9fe992b26eb05f8e0f9b7
Wafula Wanjala <wafulawanjala78@gmail.com>
2025-01-19 16:03:22 +0300

    added functionalities

--- calculator.js
+++ calculator.js
@@ -1,20 +1,45 @@
+
+let number1 = 2
 let number2 = 2
 function add(number1, number2){
-    console.log(number1+number2);
-}
-add(number1,number2);
+    return number1+number2;
+};
 
 function subtract(number1,number2){
-    console.log(number1-number2);
-}
-subtract(number1,number2);
+    return number1-number2;
+};
 
 function divide(number1, number2){
-    console.log(number1/number2)
-}
-divide(number1,number2);
+    return number1/number2;
+};
 
 function multiply(number1, number2){
-    console.log(number1*number2)
-}
-multiply(number1,number2);*/
+    return number1*number2;
+};
+
+document.getElementById('add').addEventListener('click', function() {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = add(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+});
+document.getElementById('subtract').addEventListener('click', function() {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = subtract(number1,number2)
+    document.getElementById('calculation-result').textContent = result;
+});
+
+document.getElementById('multiply').addEventListener('click', function() {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = multiply(number1,number2)
+    document.getElementById('calculation-result').textContent = result;
+});
+
+document.getElementById('divide').addEventListener('click', function() {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = divide(number1,number2)
+    document.getElementById('calculation-result').textContent = result;
+});
