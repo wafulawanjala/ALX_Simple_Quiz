9ba59c3d3c578252f4256081cf416c926a8c6287
Wafula Wanjala <wafulawanjala78@gmail.com>
2025-01-19 16:06:42 +0300

    added comments

--- calculator.js
+++ calculator.js
@@ -1,6 +1,5 @@
 
-let number1 = 2
-let number2 = 2
+//implementing arithmetic functions
 function add(number1, number2){
     return number1+number2;
 };
@@ -16,13 +15,18 @@ function divide(number1, number2){
 function multiply(number1, number2){
     return number1*number2;
 };
+ 
+//ataching event listeners
 
+// 1. Add
 document.getElementById('add').addEventListener('click', function() {
     const number1 = parseFloat(document.getElementById('number1').value) || 0;
     const number2 = parseFloat(document.getElementById('number2').value) || 0;
     const result = add(number1, number2);
     document.getElementById('calculation-result').textContent = result;
 });
+
+// 2. Subtract
 document.getElementById('subtract').addEventListener('click', function() {
     const number1 = parseFloat(document.getElementById('number1').value) || 0;
     const number2 = parseFloat(document.getElementById('number2').value) || 0;
@@ -30,6 +34,7 @@ document.getElementById('subtract').addEventListener('click', function() {
     document.getElementById('calculation-result').textContent = result;
 });
 
+//3. Multiply
 document.getElementById('multiply').addEventListener('click', function() {
     const number1 = parseFloat(document.getElementById('number1').value) || 0;
     const number2 = parseFloat(document.getElementById('number2').value) || 0;
@@ -37,6 +42,7 @@ document.getElementById('multiply').addEventListener('click', function() {
     document.getElementById('calculation-result').textContent = result;
 });
 
+//4.Divide
 document.getElementById('divide').addEventListener('click', function() {
     const number1 = parseFloat(document.getElementById('number1').value) || 0;
     const number2 = parseFloat(document.getElementById('number2').value) || 0;
