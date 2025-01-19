bee69b405935c3a06083906d576f62f6a4ff3eab
Wafula Wanjala <wafulawanjala78@gmail.com>
2025-01-19 14:56:03 +0300

    checked seleted answer

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ function checkAnswer() {
     const correctAnswer = "4"; // Ensure the correct answer matches the type (string)
     
     // Select the checked radio button
-    let ans = document. querySelector('input[name=”quiz”]:checked')
+    let ans =document.querySelector('input[name=”quiz”]:checked')
     let userAnswer = ans.value;    
     if (userAnswer === correctAnswer) {
             feedback.textcontent="Correct! Well done.";
